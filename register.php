9ebdecc462feded7e50ed3a42fbbc66a9caad0b7
Grant <grantrak94@gmail.com>
2020-12-11 16:27:17 -0700

    Completed structure and styling of the now playing bar

--- register.php
+++ register.php
@@ -18,7 +18,8 @@
 <html>
 <head>
     <title>Welcome to Slotify!</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
