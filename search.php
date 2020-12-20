b155825a90c3c6186722beb6062be4a4dadcaf41
Grant <grantrak94@gmail.com>
2020-12-19 22:50:20 -0700

    Created the search page

---
+++ search.php
@@ -0,0 +1,150 @@
+<?php 
+include("includes/includedFiles.php");
+
+if(isset($_GET['term'])) {
+    $term = urldecode($_GET['term']);
+} else {
+    $term = "";
+}
+?>
+
+
+<div class="searchContainer">
+    <h4>Search for an artist, album, or songs</h4>
+    <input type="text" class="searchInput" value="<?php echo $term; ?>"  placeholder="Start typing..." onfocus="var val=this.value; this.value=''; this.value= val;">
+</div>
+
+<script>
+    $(function() {
+        $(".searchInput").focus();
+        
+        $(".searchInput").keyup(function() {
+            clearTimeout(timer);
+            timer = setTimeout(function() {
+                var val = $(".searchInput").val();
+                openPage("search.php?term=" + val);
+            }, 2000);
+        });
+    });
+</script>
+<?php if($term == "") exit(); ?>
+<div class="tracklistContainer borderBottom">
+
+<h2>SONGS</h2>
+
+    <ul class="tracklist">
+       <?php 
+
+        $songsQuery = mysqli_query($con, "SELECT id FROM songs WHERE title LIKE '$term%' LIMIT 10");
+
+        if(mysqli_num_rows($songsQuery) == 0) {
+            echo "<span class='noResults'>No songs found matching " . $term . "<span>";
+        }
+
+        $songIdArray = array();
+
+        $i = 1;
+        while($row = mysqli_fetch_array($songsQuery)) {
+
+            if($i > 15) {
+                break;
+            }
+
+            array_push($songIdArray, $row['id']);
+            
+            $albumSong = new Song($con, $row['id']);
+            $albumArtist = $albumSong->getArtist();
+       ?>
+
+            <li class="tracklistRow">
+                <div class="trackCount">
+                    <img src="assets/images/icons/play-white.png" alt="" class="play" onclick="setTrack('<?php echo $albumSong->getId(); ?>', tempPlaylist, true) ">
+                    <span class="trackNumber"><?php echo $i; ?></span>
+                </div>
+
+                <div class="trackInfo">
+                    <span class="trackName"><?php echo $albumSong->getTitle(); ?></span>
+                    <span class="artistName"><?php echo $albumArtist->getName(); ?></span>
+                </div>
+
+                <div class="trackOptions">
+                    <img src="assets/images/icons/more.png" alt="" class="optionsButton">
+                </div>
+
+                <div class="trackDuration">
+                    <span class="duration"><?php echo $albumSong->getDuration(); ?></span>
+                </div>
+            </li>
+
+       <?php
+            $i++;
+        }
+       ?> 
+       <script>
+           var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+           tempPlaylist = JSON.parse(tempSongIds);
+       </script>
+    </ul>
+
+</div>
+
+<div class="artistsContainer borderBottom">
+     <h2>ARTISTS</h2>   
+     <?php 
+     
+     $artistsQuery = mysqli_query($con, "SELECT id FROM artists WHERE name LIKE '$term%' LIMIT 10");
+
+     if(mysqli_num_rows($artistsQuery) == 0) {
+        echo "<span class='noResults'>No artists found matching " . $term . "<span>";
+    }
+
+    while($row = mysqli_fetch_array($artistsQuery)) {
+        $artistFound = new Artist($con, $row['id']);
+    ?>
+
+       <div class="searchResultRow">
+           <div class="artistName">
+               <span role="link" tabindex="0" onclick="openpage('artist.php?id=<?php echo $artistFound->getId(); ?>')"><?php echo $artistFound->getName(); ?></span>
+           </div>
+       </div> 
+
+    <?php
+
+    }
+     
+     ?>
+</div>
+
+
+
+
+<div class="gridViewContainer">
+
+<h2>ALBUMS</h2>
+
+    <?php 
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE title like '$term%' LIMIT 10");
+
+        if(mysqli_num_rows($albumQuery) == 0) {
+            echo "<span class='noResults'>No albums found matching " . $term . "<span>";
+        }
+
+
+        while($row = mysqli_fetch_array($albumQuery)) {
+    ?>
+
+           <div class="gridViewItem">
+               <span onclick="openPage('album.php?id=<?php echo $row['id']; ?>')" role="link" tabindex="0">
+               <img src="<?php echo $row['artworkPath']; ?>" alt="">
+               <div class="gridViewInfo">
+                   <?php echo $row['title']; ?>
+               </div>
+               </span>
+           </div> 
+
+    <?php      
+        }
+    ?> 
+    
+
+</div>
